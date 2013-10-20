ba1b8d6a41d1339cca9c0c8a920993ec121c5a53
qiubix <qiubix@gmail.com>
2013-10-20 14:43:51 +0200

    Chapter 11: Dollar/Franc duplication, removed unnecessary subclasses and one unnecessary test.

--- test/example/MoneyTest.java
+++ test/example/MoneyTest.java
@@ -9,8 +9,6 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
@@ -18,10 +16,5 @@ public class MoneyTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertEquals(new Money(10,"CHF"), new Franc(10, "CHF"));
     }
 }
