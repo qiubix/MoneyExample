6cd70cd04722c34784599ba588215deea22dad90
qiubix <qiubix@gmail.com>
2013-11-06 16:16:33 +0100

    Chapter 15: changed testMixedAddition to original state, made it compile and run.

--- src/example/Money.java
+++ src/example/Money.java
@@ -22,7 +22,7 @@ class Money implements Expression{
         return new Money(multiplier * amount, currency);
     }
 
-    Expression plus(Expression addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
